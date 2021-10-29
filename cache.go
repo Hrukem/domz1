18f974b52d24fb37069485e7a924f055a92aa97d
Hrukem <junghe@yandex.ru>
2021-10-29 08:33:22 +0300

    change field CacheMap on cacheMap

--- cache.go
+++ cache.go
@@ -2,24 +2,24 @@
 package domz1
 
 type Cache struct {
-	CacheMap map[string]interface{}
+	cacheMap map[string]interface{}
 }
 
 func NewCache() Cache {
-	return Cache{CacheMap: map[string]interface{}{}}
+	return Cache{cacheMap: map[string]interface{}{}}
 }
 
 // Set function implementation add data in cache
 func (m Cache) Set(key string, value interface{}) {
-	m.CacheMap[key] = value
+	m.cacheMap[key] = value
 }
 
 // Get function implementation get data from cache
 func (m Cache) Get(key string) interface{} {
-	return m.CacheMap[key]
+	return m.cacheMap[key]
 }
 
 // Delete function implementation delete data from cache
 func (m Cache) Delete(key string) {
-	delete(m.CacheMap, key)
+	delete(m.cacheMap, key)
 }
